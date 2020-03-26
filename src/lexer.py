09c0c6ea7dd2283c947cf1513ba01c6a765fc0ae
Pedro Moura <pedrorpmoura@gmail.com>
2020-03-26 00:49:14 +0000

    Test grammar added

--- src/lexer.py
+++ src/lexer.py
@@ -1,3 +1,59 @@
 import ply.lex as lex
 import sys
+import re
+
+tokens_generated = (
+    'DOLLAR',
+    'CODE',
+    'COMMA',
+    'LSQBRACKET',
+    'DECORATOR',
+    'ACTION',
+    'NODENAME',
+    'PROB_SELECTOR',
+    'SEQUENCE',
+    'KEYWORD',
+    'DOUBLEPERCENTAGE',
+    'RSQRBRACKET',
+    'LEFTBRACKET',
+    'RIGHTBRACKET',
+    'CONDITION',
+    'COLON',
+    'BEHAVIOUR',
+    'PARALLEL',
+    'SELECTOR',
+    'LSQRBRACKET'
+)
+
+tokens = (
+    'BEHAVIOUR',
+    'SEQUENCE',
+    'SELECTOR',
+    'PROB_SELECTOR',
+    'PARALLEL',
+    'DECORATOR',
+    'ACTION',
+    'CONDITION',
+    'NODENAME',
+    'CODE',
+    'KEYWORD',
+    'DOUBLEPERCENTAGE',
+    'COMMA',
+)
+
+
+t_BEHAVIOUR = r'behaviour'
+t_SEQUENCE = r'sequence'
+t_SELECTOR = r'selector'
+t_PROB_SELECTOR = r'prob_selector'
+t_PARALLEL = r'parallel'
+t_DECORATOR = r'decorator'
+t_ACTION = r'action'
+t_CONDITION = r'condition'
+t_NODENAME = r'[a-zA-Z][a-zA-Z0-9]*'
+t_KEYWORD = r'[\[\{\]\}]'
+t_CODE = r'.*'
+t_DOUBLEPERCENTAGE = r'%%'
+t_COMMA = r'\,'
+
 
--- src/parser.py
+++ src/parser.py
@@ -1,3 +1,90 @@
 import ply.yacc as yacc
 import sys
 
+
+"""   
+
+Root : Behaviour Defs DOUBLEPERCENTAGE Code 
+    | Defs Behavior DOUBLEPERCENTAGE Code
+
+Behaviour : BEHAVIOUR LEFTBRACKET Node RIGHTBRACKET
+
+Node: Sequence,
+      Selector,
+      Prob_Selector,
+      Parallel,
+      Decorator,
+      Condition,
+      Action
+
+
+Nodes : Nodes COMMA Node
+      | Node
+
+Node : ControlFlowNode
+     | ExecutionNode
+
+ControlFlowNode : Sequence
+                | Selector
+                | ProbSelector
+                | Parallel
+                | Decorator
+
+ExecutionNode : Action
+              | Condition
+
+
+Sequence : SEQUENCE COLON LSQBRACKET Nodes RSQRBRACKET
+         | SEQUENCE COLON VarName
+
+Selector : SELECTOR COLON LSQBRACKET Nodes RSQRBRACKET
+         | SELECTOR COLON VarName
+
+ProbSelector : PROB_SELECTOR COLON LSQBRACKET ProbNodes RSQRBRACKET
+             | PROB_SELECTOR COLON VarName
+            
+Parallel : PARALLEL COLON LSQBRACKET Nodes RSQRBRACKET
+         | PARALLEL COLON VarName
+
+Decorator : DECORATOR COLON LSQBRACKET PolicyNode RSQRBRACKET
+          | DECORATOR COLON VarName
+
+Parallel : PARALLEL 
+
+ProbNodes : ProbNodes COMMA ProbNode
+          | ProbNode
+
+ProbNode : VarName RIGHTARROW Node
+
+PolicyNode : POLICY COLON Policy COMMA Node
+
+Policy : INVERTER
+       | MAX_TRIES LEFTPARENTHESIS INT RIGHTPARENTHESIS
+       | MAX_SECONDS LEFTPARENTHESIS INT RIGHTPARENTHESIS
+
+
+Action : ACTION COLON VarName
+
+Condition : CONDITION COLON VarName
+
+VarName : DOLLAR NODENAME
+
+
+Sequence : SEQUENCE COLON LSQRBRACKET Node RSQRBRACKET
+         | SEQUENCE COLON DOLLAR NODENAME
+
+Defs : Defs NodeDef
+      &
+
+NodeDef : SequenceDef,
+        | SelectorDef,
+        | ProbSelectorDef,
+        | ConditionDef,
+        | ActionDef
+         
+SequenceDef : SEQUENCE NODENAME LSQRBRACKET Nodes RSQRBRACKET
+
+Nodes : Nodes COMMA Node
+        &
+ 
+""" 
