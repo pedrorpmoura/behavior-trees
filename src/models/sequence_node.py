aca39797de84759a0a02ee9a044866cba78dbbef
Pedro Moura <pedrorpmoura@gmail.com>
2020-04-07 17:51:48 +0100

    Update sequence_node.py

--- src/models/sequence_node.py
+++ src/models/sequence_node.py
@@ -7,14 +7,14 @@ class Sequence(ControlFlowNode):
 
     def __init__(self, name, children):
         ControlFlowNode.__init__(self, name, children)
-    
-    
+
     def __str__(self):
         text = "sequence: " + str(self.name) + "\n" + "[\n"
         for child in self.children:
             text += "\t" + str(child) + "\n"
         text += "]\n"
         return text
+        
         
         
         
