1b7bc93f9bc1359e8a09649f4c207c0e4d450404
Pedro Moura <pedrorpmoura@gmail.com>
2020-06-26 15:47:12 +0100

    SLATE - corrections; started to make the game

---
+++ SLATE/revisoes.md
@@ -0,0 +1,95 @@
+# REVIEWS
+
+## Review 1
+
+<u>The state of the art presented is short, it is better to do a deep analysis for clearly perceiving the authors contribution.</u>
+
+- procurar mais informação
+
+
+
+<u>Why python was selected to do de implementation?</u> <--- FEITO(481)
+
+- mais facil de programar
+- comunidades de jogos
+- cross compiling?
+
+
+
+<u>Why lex and yacc was used?</u>  <---  FEITO(Linha 484)
+
+<u>What is the advantage of using lex-yacc respect of ANTLR?</u>
+
+<u>The concepts of attribute grammars are better supported in lex-yacc?</u> 
+
+- Consideramos ambas, mas optamos por lex yacc pq tinhamos mais experiência, e o tempo era reduzido
+- Eficiência do código gerado
+
+Ou:
+
+- Apesar dos conceitos de gramatica de atributos serem mais explicitos em ANTLR, tb são bastantes fáceis de implementar no ply já que podemos associar tipos estruturados como dicionários, que podem ser mappings entre atributos e valores.
+- A diferença entre ANTLR e yacc em C é maior que em Python
+
+
+
+<u>The bibliography is poor.</u>
+
+- meter mais algumas
+
+
+
+## Review 2
+
+<u>Include more details about the static and dynamic semantics of the DSL</u>
+
+- Descrever melhor as validações
+- Como é feito o gerador
+- $\LaTeX$ gerado
+- *Cross-compiling*
+- *Templates*
+
+
+
+## Review 3
+
+<u>It is important to stress out the use of Decision Trees together with State Machines.</u>
+
+
+
+<u>Decorators are not flow nodes. They are decorators. Usually we have "Nodes", "Flow Nodes" and "Decorators".</u> <-- FEITO(Linha 146)
+
+- even if not standard, we use decorators as control flow nodes
+- não é o foco do trabalho
+
+
+
+<u>Try not to use "don't" but "do not".</u> <--- FEITO
+
+- Corrigir 
+
+
+
+<u>Why not to generate other languages? How much that costs?</u> <--- FEITO(LINHA 558)
+
+- Referenciar em trabalho futuro
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- example/game.py
+++ example/game.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 width = 512
 height = 512
@@ -12,50 +13,136 @@ class Point():
 
 
 
-def draw_grid(screen, N):
+def generate_wall(size, orientation, position):
+    wall = []
+
+    x = position[0]
+    y = position[1]
+    for i in range(size):
+        if orientation == 0:
+            wall.append((x, y + i))
+        else:
+            wall.append((x + i, y))
+    
+    return wall
+
+
+def wall_is_valid(wall, map_dict):
+    N = int(math.sqrt(len(map_dict)))
+    for p in wall:
+        if p[0] < 1 or p[0] > N - 2 or p[1] < 1 or p[1] > N - 2:
+            return False
+
+        elif map_dict[p] != 0:
+            return False
+
+    return True
+
+
+def add_wall(wall, map_dict):
+    for p in wall:
+        map_dict[p] = 1
+
+def generate_walls(n_walls, map_dict):
+    min_wall_size = 3
+    max_wall_size = 5
+
+    for w in range(n_walls):
+        wall_size = random.randint(min_wall_size, max_wall_size)
+
+        # orientation: 0 - Horizontal, 1 - Vertical
+        orientation = random.randint(0,1)
+
+        # choose random position to create wall
+        pos = random.choice(list(map_dict.keys()))
+
+        wall = generate_wall(wall_size, orientation, pos)
+
+        while not wall_is_valid(wall, map_dict):
+            wall_size = random.randint(min_wall_size, max_wall_size)
+            orientation = random.randint(0,1)
+            pos = random.choice(list(map_dict.keys()))
+
+            wall = generate_wall(wall_size, orientation, pos)
+        
+        
+        add_wall(wall, map_dict)
+
+
+def pos_valid(pos, map_dict):
+    N = int(math.sqrt(len(map_dict)))
+    if pos[0] < 1 or pos[0] > N - 2 or pos[1] < 1 or pos[1] > N - 2:
+        return False
+
+    elif map_dict[pos]:
+        return False
+
+    return True
+
+def generate_ball(map_dict):
+    
+    pos = random.choice(list(map_dict.keys()))
+
+    while not pos_valid(pos, map_dict):
+        pos = random.choice(list(map_dict.keys()))
+
+    map_dict[pos] = 2
+
+
+
+def create_map(N):
+    d = { (i,j) : 0 for i in range(N) for j in range(N) }
+
+    generate_walls(8, d)
+    generate_ball(d)
+
+    return d
+
+
+def draw_map(map_dict, screen, N):
     n = width / N
     
-    for i in range(N):
-        for j in range(N):
-            
-            p = random.randint(0,1)
-            if p == 0:
-                color = (51, 102, 0)
-            elif p == 1:
-                color = (0, 102, 0)
-            
-            pygame.draw.rect(screen, color, (i * n, j * n, n, n))
+    for (x,y) in list(map_dict.keys()):
 
-    return n
+        if map_dict[(x,y)] == 1:
+            color = (0,0,0)
+        elif map_dict[(x,y)] == 2:
+            color = (255,0,0)
+        elif map_dict[(x,y)] == 3:
+            color = (0,0,255)
+        elif (x + y) % 2 == 0:
+            color = (112,128,144)
+        else:
+            color = (119,136,153)
+
+        pygame.draw.rect(screen, color, (x * n, y * n, n, n))
+    
 
 
-def draw_house(screen, n):
-
-    for i in range(5, 12):
-        pygame.draw.rect(screen, (0,0,0), (i * n, 2 * n, n, n))
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((width,height))
 
-    pygame.display.set_caption("PAC-MAN")
+    pygame.display.set_caption("GAME")
 
     pygame.time.delay(100)
 
-    n = draw_grid(screen, 16)
-
-    draw_house(screen, n)
-
+    N = 16
+    game_dict = create_map(N)
+    draw_map(game_dict, screen, N)
+    
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        
-
         pygame.display.update()
+
+
+
 
 
 if __name__ == "__main__":
